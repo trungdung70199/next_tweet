441d2eef4f0d60cd424fc9967ced592d8e6e22d6
trungdung70199 <trungdung70199@gmail.com>
2024-01-15 11:58:18 +0900

    update

--- server/app/Http/Controllers/Api/RegistUserController.php
+++ server/app/Http/Controllers/Api/RegistUserController.php
@@ -4,13 +4,8 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\RegistUserRequest;
-use Illuminate\Http\Request;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Log;
-use Illuminate\Support\Str;
-use Exception;
 
 class RegistUserController extends Controller
 {
@@ -23,9 +18,10 @@ class RegistUserController extends Controller
             'password' => Hash::make($request->password),
         ]);
         if ($user) {
-            $user->remember_token = $user->createToken('auth_token', '[*]', now()->addWeek())->plainTextToken;;
+            $user->remember_token = $user->createToken('auth_token', ['*'], now()->addWeek())->plainTextToken;
             $user->save();
-            return response()->json(['user' => $user, 'access_token' => $user->remember_token]);
+            $user->access_token = $user->remember_token;
+            return response()->json(['user' => $user]);
         } else {
             return response()->json(['error' => ['message' => 'invalid regist']]);
         }
