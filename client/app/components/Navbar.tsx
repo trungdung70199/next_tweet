67f442fe4f5783525ac3e2d30a5185912ed95885
trungdung70199 <trungdung70199@gmail.com>
2024-01-15 13:36:59 +0900

    update navbar

--- client/app/components/Navbar.tsx
+++ client/app/components/Navbar.tsx
@@ -6,8 +6,8 @@ const Navbar = () => {
     return (
         <nav>
             <div className='flex flex-wrap items-center mx-auto py-4'>
+                <SiLoopback className="me-3" size="2em" />
                 <Link href="/" className='me-3'>
-                    <SiLoopback/>
                     <span className='self-center text-2xl font-semibold'>Next Tweet</span>
                 </Link>
 
