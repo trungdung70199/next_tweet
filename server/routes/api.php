85a8f040f168c0146659650061131e6f75a625a0
trungdung70199 <trungdung70199@gmail.com>
2024-01-15 10:01:10 +0900

    update api

--- server/routes/api.php
+++ server/routes/api.php
@@ -1,16 +1,19 @@
 <?php
 
 use App\Http\Controllers\Api\TweetController;
-use App\Models\Tweet;
+use App\Http\Controllers\Api\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-// Routing url: [/api/xxx]
 
+//ルーティングのURL：「/api/xxxx」
+// TweetController
 Route::get('tweet/get', [TweetController::class, 'get']);
 Route::post('tweet/add', [TweetController::class, 'add']);
+
+// AuthController
+Route::post('/auth', [AuthController::class, 'auth']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-
