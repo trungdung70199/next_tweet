0453828721ffc5b496171e1327f5a6a33446a0d4
trungdung70199 <trungdung70199@gmail.com>
2024-01-16 09:42:55 +0900

    add

---
+++ client/app/auth/register/page.tsx
@@ -0,0 +1,14 @@
+import { FaUser } from "react-icons/fa";
+
+const RegistPage = () => {
+    return (
+        <div>
+            <h1 className=" p-3 flex justify-center text-3xl font-bold">
+                <FaUser className="mt-1 me-3" />
+                Sign up
+            </h1>
+        </div>
+    );
+}
+
+export default RegistPage;
