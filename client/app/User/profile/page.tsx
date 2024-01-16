cf7c95ef5e3dfc7960ee02d262b60b52c00d86c0
trungdung70199 <trungdung70199@gmail.com>
2024-01-16 09:32:38 +0900

    update

--- client/app/User/profile/page.tsx
+++ client/app/User/profile/page.tsx
@@ -1,12 +1,12 @@
-import Image from "next/image";
-import White from '@/public/img/White.jpeg';
+import Image from 'next/image';
+import imageMe from '@/public/images/me.png';
 
 const ProfilePage = () => {
     return (
         <div>
-            <h1 className="text-3xl text-center font-bold">Profile</h1>
+            <h1 className="text-3xl text-center font-bold p-3">Profile</h1>
             <div className="flex justify-center p-3">
-                <Image src={White} className="h-36 w-36 rounded-full" alt={""} />
+                <Image src={imageMe} className="h-36 w-36 rounded-full" alt="" />
             </div>
             <div className="flex justify-center p-3">
                 Dummy User
