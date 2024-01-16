6ea8b787484942db2d1fd3b972d0cbf46a027342
trungdung70199 <trungdung70199@gmail.com>
2024-01-16 09:30:15 +0900

    add page & img

--- client/app/User/profile/page.tsx
+++ client/app/User/profile/page.tsx
@@ -1,7 +1,16 @@
+import Image from "next/image";
+import White from '@/public/img/White.jpeg';
+
 const ProfilePage = () => {
     return (
         <div>
-            <h2>Profile</h2>
+            <h1 className="text-3xl text-center font-bold">Profile</h1>
+            <div className="flex justify-center p-3">
+                <Image src={White} className="h-36 w-36 rounded-full" alt={""} />
+            </div>
+            <div className="flex justify-center p-3">
+                Dummy User
+            </div>
         </div>
     );
 }
