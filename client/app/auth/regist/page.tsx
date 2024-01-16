97a876c78f725cd77d07181f05667f4eb1c84b75
trungdung70199 <trungdung70199@gmail.com>
2024-01-16 11:02:05 +0900

    update

--- client/app/auth/regist/page.tsx
+++ client/app/auth/regist/page.tsx
@@ -1,20 +1,54 @@
 "use client"
 
+import { useState } from "react";
 import { FaUser } from "react-icons/fa";
-import Input from "../../components/Input";
+import Link from "next/link";
+import Input from "@/app/components/Input";
 
 const RegistPage = () => {
+    const [name, setName] = useState("");
+
+    const regist = async () => {
+        alert(name)
+        console.log(name)
+    }
+
     return (
-        <div>
-            <h1 className="p-3 flex justify-center text-3xl font-bold">
-                <FaUser className="mt-1 me-3" />
+        <div className="mx-auto w-1/3">
+            <h1 className="my-2 p-1 flex justify-center text-2xl font-bold">
+                <FaUser className="mt-2 me-2" />
                 Sign up
             </h1>
 
             <div>
-                <Input type="text" placeholder="Your Name" />
+                <Input type="text" placeholder="Your Name" onChange={setName} />
                 <Input type="text" placeholder="Email" />
                 <Input type="password" placeholder="******" />
+            </div>
+
+            <div>
+                <button
+                    onClick={regist}
+                    className="
+                            w-full
+                          bg-blue-500 hover:bg-blue-700
+                          text-white font-bold 
+                          py-3 px-4 mb-2
+                          rounded
+                         ">
+                    Sign up
+                </button>
+                <Link
+                    href="/auth/login"
+                    className="
+                            flex justify-center
+                          bg-gray-200 hover:bg-gray-300
+                          text-gray-500 font-bold 
+                          py-3 px-4 
+                          rounded
+                         ">
+                    Sing in
+                </Link>
             </div>
         </div>
     );
