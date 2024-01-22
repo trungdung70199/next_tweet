e5ca73d60064a687252a4d3fc92026a3d3bbd824
trungdung70199 <trungdung70199@gmail.com>
2024-01-22 14:13:59 +0900

    update

--- client/app/services/TweetServices.ts
+++ client/app/services/TweetServices.ts
@@ -6,7 +6,7 @@ const LARAVEL_API_URL = process.env.NEXT_PUBLIC_LARAVEL_API_URL;
 export const getTweets = async (accessToken: string) => {
     const url = LARAVEL_API_URL + "tweet/get"
     const response = await fetch(url, {
-        method: "POST",
+        method: "GET",
         headers: {
             'Authorization': `Bearer ${accessToken}`,
             'Content-Type': 'application/json',
@@ -18,6 +18,18 @@ export const getTweets = async (accessToken: string) => {
 }
 
 // Tweet投稿
-export const postTweet = (user: User, message: string) => {
-
+export const postTweet = async (user: User, message: string) => {
+    const url = LARAVEL_API_URL + "tweet/add";
+    const user_id = user.id;
+    const response = await fetch(url, {
+        method: "POST",
+        headers: {
+            'Authorization': `Bearer ${user.accessToken}`,
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ message, user_id }),
+    })
+    if (response.ok) {
+        return await response.json();
+    }
 }
