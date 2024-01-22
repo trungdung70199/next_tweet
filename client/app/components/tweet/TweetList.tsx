c8b1aad863bd69bcf93ed72db208429670993ac3
trungdung70199 <trungdung70199@gmail.com>
2024-01-22 11:56:17 +0900

    add TweetList

---
+++ client/app/components/tweet/TweetList.tsx
@@ -0,0 +1,29 @@
+"use client"
+
+import { useEffect, useState, } from 'react';
+import TweetDetail from '@/app/components/tweet/TweetDetail';
+import { Tweet } from '@/app/models/Tweet';
+
+interface TweetListProps {
+    initialTweets: Tweet[];
+}
+
+const TweetList = ({ initialTweets, }: TweetListProps) => {
+    const [tweets, setTweets] = useState<Tweet[]>(initialTweets);
+
+    useEffect(() => {
+        setTweets(initialTweets);
+    }, [initialTweets]);
+
+    return (
+        <div>
+            {
+                tweets?.map((tweet) => (
+                    <TweetDetail key={tweet.id} tweet={tweet} />
+                ))
+            }
+        </div>
+    );
+}
+
+export default TweetList;
