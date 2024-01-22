2bbe5ebee3e4a651d1c3028d5797f3880af2344a
trungdung70199 <trungdung70199@gmail.com>
2024-01-22 14:13:16 +0900

    update

--- client/app/components/tweet/TweetForm.tsx
+++ client/app/components/tweet/TweetForm.tsx
@@ -1,27 +1,40 @@
+import { User, testUser } from "@/app/models/User";
+import { postTweet } from "@/app/services/TweetServices";
 import { useState } from "react";
 
 const TweetForm = () => {
+    const [user, setUser] = useState<User>(testUser)
+
     const [message, setMessage] = useState<string>("")
+    const [isButtonDisabled, setIsButtonDisabled] = useState(true);
+    const enableButtonClassName = "bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg";
+    const disableButtonClassName = "bg-blue-200 text-white font-bold py-2 px-4 rounded-lg";
 
     const messageHandler = (event: React.ChangeEvent<HTMLTextAreaElement>): void => {
-        console.log(event.target.value)
+        setMessage(event.target.value)
+        setIsButtonDisabled(event.target.value.length == 0);
+    }
+
+    function onPost(): void {
+        postTweet(user, message)
+        setMessage("");
+        setIsButtonDisabled(true);
     }
 
     return (
         <div>
-            <textarea 
+            <textarea
+                value={message}
                 onChange={messageHandler}
                 className="resize-none 
                     w-full h-24 border rounded-md p-2"
                 placeholder="今なにしてる？"></textarea>
-
-            <button 
-                className="bg-blue-500 hover:bg-blue-700 
-                    text-white font-bold 
-                    py-2 px-4 
-                    rounded-lg">
-                Send
-            </button>
+            <div className="p-3">{message.length} characters.</div>
+            <button
+                onClick={onPost}
+                className={isButtonDisabled ? disableButtonClassName : enableButtonClassName}
+                disabled={isButtonDisabled}>
+                Send</button>
         </div>
     );
 }
