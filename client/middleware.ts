d7016fd996bbb8862e14fcef1ba3a7f33e3d6caf
trungdung70199 <trungdung70199@gmail.com>
2024-01-31 19:25:26 +0900

    update middleware

--- client/middleware.ts
+++ client/middleware.ts
@@ -1,15 +1,21 @@
-import { NextRequest, NextResponse } from 'next/server';
-
-const authURL = process.env.BASE_URL + "auth/login";
-
-export async function middleware(req: NextRequest) {
-    console.log('--- middleware ---')
-    const token = req.cookies.get('access_token');
-    console.log(token)
-    if (!token) return NextResponse.redirect(authURL);
-    return NextResponse.next();
-}
+export { default } from "next-auth/middleware";
 
 export const config = {
     matcher: ["/", "/user/:path*", "/user/:slug*"]
 };
+
+// import { NextRequest, NextResponse } from 'next/server';
+
+// const authURL = process.env.BASE_URL + "auth/login";
+
+// export async function middleware(req: NextRequest) {
+//     console.log('--- middleware ---')
+//     const token = req.cookies.get('access_token');
+//     console.log(token)
+//     if (!token) return NextResponse.redirect(authURL);
+//     return NextResponse.next();
+// }
+
+// export const config = {
+//     matcher: ["/", "/user/:path*", "/user/:slug*"]
+// };
