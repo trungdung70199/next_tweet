591f7096e9eae4e1eddf25d7e1de81915d8f3c03
trungdung70199 <trungdung70199@gmail.com>
2024-01-31 19:46:57 +0900

    update

---
+++ client/app/api/auth/route.ts
@@ -0,0 +1,82 @@
+import { redirect } from 'next/navigation';
+import NextAuth, { NextAuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import GitHubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
+import AppleProvider from "next-auth/providers/apple";
+import FacebookProvider from "next-auth/providers/facebook";
+
+import { signIn, getUser, signInForSNS } from '@/app/services/UserService';
+import { Session } from "next-auth";
+import { SNSAccount } from '@/app/models/User';
+
+export const authOptions: NextAuthOptions = {
+    debug: false,
+    pages: {
+        signIn: '/auth/login',
+        error: '/auth/login',
+    },
+    session: { strategy: "jwt" },
+    providers: [
+        GitHubProvider({
+            clientId: process.env.GITHUB_ID!,
+            clientSecret: process.env.GITHUB_SECRET!,
+        }),
+        // GoogleProvider({
+        //     clientId: process.env.GOOGLE_CLIENT_ID!,
+        //     clientSecret: process.env.GOOGLE_CLIENT_SECRET!
+        // }),
+        CredentialsProvider({
+            name: "Sign in",
+            credentials: {
+                email: { label: 'Email', type: 'email', placeholder: 'Email' },
+                password: { label: 'Password', type: 'password', placeholder: 'Password' }
+            },
+            async authorize(credentials) {
+                console.log("authorize():", credentials)
+                if (!credentials?.email || !credentials?.password) return;
+
+                const email = credentials?.email || "";
+                const password = credentials?.password || "";
+                const result = await signIn({ email, password });
+
+                if (result?.access_token) {
+                    const user = getUser(result?.access_token);
+                    return user;
+                }
+                return false;
+            },
+        }),
+    ],
+    callbacks: {
+        async signIn({ user, account, profile, email, credentials }) {
+            console.log('--- signIn ---')
+            console.log(user, account, profile, email, credentials)
+            if (user && account) {
+                const authUser:SNSAccount = {
+                    name: user.name || "",
+                    email: user.email || "",
+                    provider: account.provider || "",
+                    providerAccountId: account.providerAccountId || "",
+                    accessToken: account.access_token || "",
+                };
+                await signInForSNS(authUser)
+            }
+            return true;
+        },
+        async redirect({ url, baseUrl }) {
+            return baseUrl
+        },
+        async jwt({ token, user }) {
+            return { ...token, ...user }
+        },
+        async session({ session, token }) {
+            session.user = token as Session["user"];
+            return session;
+        },
+    }
+};
+
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST }
--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -1,25 +1,32 @@
 import type { Metadata } from 'next'
 import './globals.css'
-import Navbar from '@/app/components/Navbar'
+import Navbar from '@/app/components/Navbar';
+import UserProvider from "@/app/providers/UserProvider"
+import { NextAuthProvider } from './providers/NextAuthProvider';
+
+// import AuthContext from './context/UserContext';
 
 export const metadata: Metadata = {
   title: 'Next Tweet',
-  description: 'This is tweet app',
+  description: 'This is tweet app sample.',
 }
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode,
 }) {
-  // JSX
   return (
     <html lang="ja">
-      <body className='flex min-h-screen flex-col p-2'>
-        <Navbar />
-        <main className="m-10">
-          {children}
-        </main>
+      <body>
+        {/* <UserProvider> */}
+        <NextAuthProvider>
+          <Navbar />
+          <main className="flex min-h-screen flex-col p-5">
+            {children}
+          </main>
+        </NextAuthProvider>
+        {/* </UserProvider> */}
       </body>
     </html>
   )
